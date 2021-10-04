ba68b8294507ddc5312bdfdf1e8b1f58384dc7e2
Rajesh T <rajesh.es89@gmail.com>
2021-10-04 22:44:08 +0530

    Update test_basic.py

--- test_basic.py
+++ test_basic.py
@@ -1,5 +1,5 @@
 import param_lib
-def test_iperf_tcp(srv_hdl,tc_param):
+def test_basic(srv_hdl,tc_param):
     dict_name = inspect.stack()[0][3]
     dict_param,log = param_lib.init_variables(dict_name,tc_param,srv_hdl)
 
